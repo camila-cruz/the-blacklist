33bcb2ba1169a15543c271f96d513e0edec5eb83
Camila <cruz.camila001@gmail.com>
2020-10-24 22:09:58 -0300

    Creates the list visualization page

---
+++ src/pages/List/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Container from '../../components/Container';
+
+export default function List() {
+    const [listEpisodes, setList] = useState([]);
+
+    useEffect(() => {
+        const overall_episode = localStorage.getItem('overall_episode');
+
+        axios.get(`http://localhost:5000/${overall_episode}`).then((response) =>{
+            console.log('aaaaa')
+            setList(response);
+        });
+
+    }, []);
+
+    // function sortList() {
+    //     JSON.sort(function(a, b){
+    //         return a.blacklist_guide[0] - b.blacklist_guide[0];
+    //     });
+    // }
+
+    return (
+        <Container>
+            <h2>The list</h2>
+            {listEpisodes}
+        </Container>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import List from './pages/List';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/list" exact component={List} />
             </Switch>
         </BrowserRouter>
     );
