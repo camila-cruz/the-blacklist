1ed242e1418bf2b400a08eb273cc0bfa346348c5
Camila <cruz.camila001@gmail.com>
2020-10-30 22:49:53 -0300

    Adds the env variable for vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_FUNCTIONS_BASE_URL || process.env.REACT_APP_BASE_URL,
+    // baseURL: process.env.REACT_APP_BASE_URL,
     // baseURL: 'http://localhost:5000/',
 });
 
