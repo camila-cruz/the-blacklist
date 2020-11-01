79f4cc4e803392193e9cca4a937f16b56d567640
Camila <cruz.camila001@gmail.com>
2020-11-01 17:46:51 -0300

    Adds error handling to the list

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
 
+import './index.css';
+
 export default function List() {
     const [blacklist, setList] = useState([]);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         const overall_episode = localStorage.getItem('overall_episode');
 
         // axios.get(process.env.NEXT_PUBLIC_FUNCTIONS_API_URL + `/${overall_episode}`).then((response) =>{
-        api.get(`${overall_episode}`).then((response) =>{
+        api.get(`/${overall_episode}`).then((response) =>{
             console.log(response.data)
             setList(response.data);
+        }).catch(err => {
+            console.error(err);
+            setError(err);
         });
 
     }, []);
 
-    // function sortList() {
+    // function sortList(list) {
     //     JSON.sort(function(a, b){
     //         return a.blacklist_guide[0] - b.blacklist_guide[0];
     //     });
@@ -26,16 +32,17 @@ export default function List() {
 
     return (
         <Container>
-            <h2>The list</h2>
-            <ul>
+            <h2 className="listTitle">The list</h2>
+            <ul className="blacklist">
                 {
-                    blacklist.map(blacklister => {
+                    (Array.isArray(blacklist) && blacklist.map(blacklister => {
                         return (
                             <li key={blacklister.blacklist_guide[0]}>
-                                {blacklister.title}
+                                {`${blacklister.blacklist_guide[0]} - ${blacklister.title}`}
                             </li>
                         )
-                    })
+                    })) || (error &&
+                    (<h1>Ops... an error has occurred</h1>))
                 }
             </ul>
         </Container>
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_FUNCTIONS_BASE_URL || process.env.REACT_APP_BASE_URL,
-    // baseURL: process.env.REACT_APP_BASE_URL,
     // baseURL: 'http://localhost:5000/',
 });
 
