5f258c494a554ecfcebbdf3be41f84edf1d25f57
Camila <cruz.camila001@gmail.com>
2020-11-02 20:47:20 -0300

    Creates the list view with redacted names

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
@@ -8,43 +7,116 @@ import './index.css';
 
 export default function List() {
     const [blacklist, setList] = useState([]);
-    const [error, setError] = useState(null)
+    const [viewableList, setViewableList] = useState([]);
 
     useEffect(() => {
         const overall_episode = localStorage.getItem('overall_episode');
 
-        // axios.get(process.env.NEXT_PUBLIC_FUNCTIONS_API_URL + `/${overall_episode}`).then((response) =>{
-        api.get(`/${overall_episode}`).then((response) =>{
+        api.get(`${overall_episode}`).then((response) => {
             console.log(response.data)
             setList(response.data);
         }).catch(err => {
             console.error(err);
-            setError(err);
         });
 
     }, []);
 
-    // function sortList(list) {
-    //     JSON.sort(function(a, b){
-    //         return a.blacklist_guide[0] - b.blacklist_guide[0];
-    //     });
-    // }
+    useEffect(() => {
+        (blacklist.length > 0) && numberToArrayOfObjects(184);      // TODO: get number of lowest blacklisterfrom server
+    }, [blacklist]);
+
+    function numberToArrayOfObjects(num) {
+        let list = [], currentNumber = 0;
+
+        for (let i = 0; i < num; i++) {
+            let objAdded = false, allIndexesChecked = false;
+            
+            if (currentNumber < blacklist.length) {
+                let blacklister = blacklist[currentNumber]; 
+                
+                for (let j = 0; j < blacklister.blacklist_guide.length; j++) {
+                    if (blacklister.blacklist_guide[j] === i+1) {
+                        list.push({
+                            ...blacklister,
+                            blacklist_guide: [blacklister.blacklist_guide[j]]
+                        });
+                        
+                        objAdded = true;
+                    }
+                    if (j === blacklister.blacklist_guide.length - 1) {
+                        allIndexesChecked = true;
+                    } else if (objAdded) {
+                        break;
+                    }
+                }
+            }
+
+            if (!objAdded) {
+                list.push({
+                    'blacklist_guide': [i+1],
+                    'title': '█'.repeat(getRandomArbitrary(8,15))
+                })
+            }
+            if (objAdded && allIndexesChecked) {
+                currentNumber++;
+            }
+        }
+
+        setViewableList(list);
+    }
+
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    if (!blacklist) {
+        return (
+            <Container>
+                <h1>Carregando...</h1>
+            </Container>
+        ) 
+    }
 
     return (
         <Container>
             <h2 className="listTitle">The list</h2>
             <ul className="blacklist">
                 {
-                    (Array.isArray(blacklist) && blacklist.map(blacklister => {
+                    viewableList.map(blacklister => {
                         return (
                             <li key={blacklister.blacklist_guide[0]}>
                                 {`${blacklister.blacklist_guide[0]} - ${blacklister.title}`}
                             </li>
                         )
-                    })) || (error &&
-                    (<h1>Ops... an error has occurred</h1>))
+                    })
                 }
             </ul>
         </Container>
     );
 }
+
+// (Array.isArray(blacklist) && blacklist.map(blacklister => {
+//     for (let i = 0; i < blacklister.blacklist_guide.length; i++) {
+//         while (blacklister.blacklist_guide[i] !== currentNumber) {
+//             currentNumber++;
+//             console.log(currentNumber)
+//             return (
+//                 <li key={currentNumber}>
+//                     {`${currentNumber} - -------------------------------`}
+//                 </li>
+//             )
+//         }
+//         currentNumber++;
+//         return (
+//             <li key={blacklister.blacklist_guide[i]}>
+//                 {`${blacklister.blacklist_guide[i]} - ${blacklister.title}`}
+//             </li>
+//         )
+//     }
+    // return (
+    //     <li key={blacklister.blacklist_guide[0]}>
+    //         {`${blacklister.blacklist_guide[0]} - ${blacklister.title}`}
+    //     </li>
+    // )
+// })) || (error &&
+// (<h1>Ops... an error has occurred</h1>))
